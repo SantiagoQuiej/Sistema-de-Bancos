09973e4e76cb8260ee6494a620ef669696cb5f8a
Lucas Quiej <quiej@gmail.com>
2026-02-20 17:05:34 -0600

    Primer commit del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route} from "react-router-dom";
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-100">
+        Tailwind funcionando🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
